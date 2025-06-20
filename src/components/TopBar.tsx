ef5cace2a38650c00b7517c9fc3fb8d2f845b0d6
Justsharon <odiwasharon@gmail.com>
2025-06-20 22:29:48 +0300

    clean up un used features

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify/react";
 import Hamburger from "./Hamburger";
+
 interface TopbarProps {
   isSidebarOpen: boolean;
   toggleSidebar: () => void;
@@ -7,8 +8,8 @@ interface TopbarProps {
 
 const TopBar: React.FC<TopbarProps> = ({isSidebarOpen, toggleSidebar}) => {
   return (
-    <header className="bg-white shadow p-4 flex justify-between items-center">
-      <nav className="flex items-center space-x-2">
+    <header className="bg-white shadow p-4 flex justify-end items-center">
+      <nav className="flex items-end space-x-2">
         <Icon
           icon="arcticons:mjweather"
           height="24"
@@ -17,12 +18,12 @@ const TopBar: React.FC<TopbarProps> = ({isSidebarOpen, toggleSidebar}) => {
         <p className="hidden md:block font-bold text-xl text-gray-900">Weather Dashboard</p>
         <Hamburger isOpen={isSidebarOpen} onClick={toggleSidebar} />
       </nav>
-      <div className="flex items-center space-x-4">
+      {/* <div className="flex items-center space-x-4">
         <span className="text-sm text-gray-500">Logged in</span>
         <button className="bg-gray-900 text-sm text-white px-3 py-1 rounded hover:bg-blue-600">
           Logout
         </button>
-      </div>
+      </div> */}
     </header>
   );
 };
